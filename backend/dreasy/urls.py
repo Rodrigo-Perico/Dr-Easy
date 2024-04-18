acc07e14ab7150ff8694cd23dc332980f8b162b8
Luiz Zamudio <luizaugustozamudio@gmail.com>
2024-04-17 21:12:16 -0300

    feat(backend): backend added

--- backend/dreasy/urls.py
+++ backend/dreasy/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for dreasy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("fulltexto.urls"))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
